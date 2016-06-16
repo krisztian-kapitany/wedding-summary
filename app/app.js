fb402a6232a544af1b96206277ef4f111a3873eb
krisztian-kapitany <Krisztian_Kapitany@epam.com>
2016-06-16 15:49:25 +0200

    Finished with filtering options

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ angular.module('resultApp', []).controller('RestultsTableContoller', function($h
   }.bind(this);
   this.refreshResults();
   
+  //Specific filter functions
   this.filter_attending= function(entry){
     return entry.attending === "true";
   };
@@ -21,9 +22,49 @@ angular.module('resultApp', []).controller('RestultsTableContoller', function($h
     return entry.food.vegetarian > 0;
   };
   
-  //Filtering General
-  this.filteredResults = [];
+  this.filter_szkiraly = function(entry){
+    return entry.attending === "true" 
+    && entry.accomodation.need_accomodation === "true" 
+    && entry.accomodation.hotel === "szkiraly";
+  };
   
+  this.filter_park = function(entry){
+    return entry.attending === "true" 
+    && entry.accomodation.need_accomodation === "true" 
+    && entry.accomodation.hotel === "park";
+  };
+  
+  this.filter_fenyo = function(entry){
+    return entry.attending === "true" 
+    && entry.accomodation.need_accomodation === "true" 
+    && entry.accomodation.hotel === "fenyo";
+  };
+  
+  this.filter_siesta = function(entry){
+    return entry.attending === "true" 
+    && entry.accomodation.need_accomodation === "true" 
+    && entry.accomodation.hotel === "siesta";
+  };
+  
+  this.filter_car = function(entry){
+    return entry.transport_options.some(function(opt){
+      return opt.option === "car";
+    });
+  };
+  
+  this.filter_bus = function(entry){
+    return entry.transport_options.some(function(opt){
+      return opt.option === "bus";
+    }); 
+  };
+  
+  this.filter_train = function(entry){
+    return entry.transport_options.some(function(opt){
+      return opt.option === "train";
+    });
+  };
+  
+  //Filtering General
   this.filterResults = function(filter_func){
     this.resultsData = this.resultsData.filter(filter_func);
   }.bind(this);
